2f58b59bd4d2f30b1bf572b74c482699fd27fe4f
sagarkalsariya08 <sagarkalsariya09@gmail.com>
2025-04-18 15:45:33 +0530

    Deploy to github

--- src/main.jsx
+++ src/main.jsx
@@ -12,7 +12,7 @@ createRoot(document.getElementById('root')).render(
     <UserProvider>
       <Cartprovider>
         <Productprovider>
-          <BrowserRouter>
+          <BrowserRouter basename='/react-ecomerece'>
             <App />
           </BrowserRouter>
         </Productprovider>
